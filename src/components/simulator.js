c4d484d03925873d9a9ef01a22584661d9d5fd18
Ben Larson <10101828+ben-larson@users.noreply.github.com>
2020-06-20 18:52:45 -0500

    Set up and run prettier (#8)

--- src/components/simulator.js
+++ src/components/simulator.js
@@ -5,8 +5,8 @@ import {
   Events,
   Render,
   Vertices,
-  World,
-} from "matter-js";
+  World
+} from 'matter-js';
 import debtPayoff from '../models/debtPayoff';
 
 const startX = 0;
@@ -20,41 +20,37 @@ const setup = () => {
   const debtPayoffCalendar = debtPayoff();
   const hillSize = monthHillLength * debtPayoffCalendar.length;
 
-  hill = Bodies.rectangle(startX + hillSize / 2, startY,
-    hillSize,
-    50,
-    {
-      isStatic: true,
-      angle: 0.3,
-    }
-  );
-}
+  hill = Bodies.rectangle(startX + hillSize / 2, startY, hillSize, 50, {
+    isStatic: true,
+    angle: 0.3
+  });
+};
 
 export const start = () => {
   setup();
 
   const vertices = Vertices.create([
     { x: 0, y: 0 },
-    { x: 800, y: 600 },
+    { x: 800, y: 600 }
   ]);
   const bounds = Bounds.create(vertices);
   const engine = Engine.create();
   const render = Render.create({
     bounds,
-    element: document.getElementById("PhysicsWorld"),
+    element: document.getElementById('PhysicsWorld'),
     engine,
     options: {
       hasBounds: true,
       width: 800,
       height: 600,
       wireframes: false,
-      background: "#61d5ff",
-    },
+      background: '#61d5ff'
+    }
   });
 
   World.add(engine.world, [hill, snowball]);
 
-  Events.on(engine, "beforeUpdate", (event) => {
+  Events.on(engine, 'beforeUpdate', (event) => {
     render.bounds.min.x = snowball.bounds.min.x - 200;
     render.bounds.min.y = snowball.bounds.min.y - 200;
     render.bounds.max.x = snowball.bounds.min.x + 600;
