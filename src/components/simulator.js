31e0efb9599fca8d6a20d13695be4682fb98bd28
Samuel Schumacher <samschu@live.com>
2020-06-20 20:52:12 -0500

    Dynamic hill length (#7)
    * dynamic hill length
    
    * Set up and run prettier (#8)
    
    Co-authored-by: Ben Larson <10101828+ben-larson@users.noreply.github.com>

--- src/components/simulator.js
+++ src/components/simulator.js
@@ -5,40 +5,52 @@ import {
   Events,
   Render,
   Vertices,
-  World,
-} from "matter-js";
+  World
+} from 'matter-js';
+import debtPayoff from '../models/debtPayoff';
 
-const snowball = Bodies.circle(100, 200, 40);
-const box = Bodies.rectangle(450, 50, 100, 100);
-const ground = Bodies.rectangle(400, 610, 900, 300, {
-  isStatic: true,
-  angle: 0.2,
-});
+const startX = 0;
+const startY = 400;
+const monthHillLength = 200;
+
+const snowball = Bodies.circle(startX + 40, 200, 40);
+let hill = [];
+
+const setup = () => {
+  const debtPayoffCalendar = debtPayoff();
+  const hillSize = monthHillLength * debtPayoffCalendar.length;
+
+  hill = Bodies.rectangle(startX + hillSize / 2, startY, hillSize, 50, {
+    isStatic: true,
+    angle: 0.3
+  });
+};
 
 export const start = () => {
+  setup();
+
   const vertices = Vertices.create([
     { x: 0, y: 0 },
-    { x: 800, y: 600 },
+    { x: 800, y: 600 }
   ]);
   const bounds = Bounds.create(vertices);
-
   const engine = Engine.create();
   const render = Render.create({
     bounds,
-    element: document.getElementById("PhysicsWorld"),
-    engine: engine,
+    element: document.getElementById('PhysicsWorld'),
+    engine,
     options: {
       hasBounds: true,
       width: 800,
       height: 600,
       wireframes: false,
-      background: "#61d5ff",
-    },
+      background: '#61d5ff'
+    }
   });
 
-  World.add(engine.world, [snowball, box, ground]);
+  World.add(engine.world, [hill, snowball]);
 
-  Events.on(engine, "beforeUpdate", (event) => {
+  Events.on(engine, 'beforeUpdate', (event) => {
     render.bounds.min.x = snowball.bounds.min.x - 200;
     render.bounds.min.y = snowball.bounds.min.y - 200;
     render.bounds.max.x = snowball.bounds.min.x + 600;
---
+++ src/models/debtPayoff.js
@@ -0,0 +1,60 @@
+const debts = [
+  {
+    name: 'Medical Bill',
+    minimumPayment: 50,
+    total: 500
+  },
+  {
+    name: 'Credit Card',
+    minimumPayment: 63,
+    total: 2500
+  },
+  {
+    name: 'Car Loan',
+    minimumPayment: 135,
+    total: 7000
+  },
+  {
+    name: 'Student Loan',
+    minimumPayment: 96,
+    total: 10000
+  }
+];
+
+// returns an array of months, containing
+//  - current extra payment
+//  - array of paid off debts
+//    - debt name
+//    - debt total
+export default (debts, extraPayment) => [
+  {
+    currentExtraPayment: 500,
+    paidOffDebts: [
+      {
+        name: 'Medical Bill',
+        total: 500
+      }
+    ]
+  },
+  {
+    currentExtraPayment: 550,
+    paidOffDebts: []
+  },
+  {
+    currentExtraPayment: 550,
+    paidOffDebts: []
+  },
+  {
+    currentExtraPayment: 550,
+    paidOffDebts: []
+  },
+  {
+    currentExtraPayment: 550,
+    paidOffDebts: [
+      {
+        name: 'Credit Card',
+        total: 2500
+      }
+    ]
+  }
+];
