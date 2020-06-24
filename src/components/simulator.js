dd9ef16049350e5930eaf5af80c79f5acaf1e701
Samuel Schumacher <samschu@live.com>
2020-06-24 18:38:27 -0500

    stuff

--- src/components/simulator.js
+++ src/components/simulator.js
@@ -13,18 +13,45 @@ import debtPayoff from '../models/debtPayoff';
 let currentUpdate = 0;
 let currentMonthIndex = 0;
 let debtPayoffCalendar;
-const startX = 0;
+const snowballStartingSize = 20;
+const startX = 100;
 const startY = 400;
-const monthHillLength = 200;
+const monthHillLength = snowballStartingSize * 5;
 
-const snowball = Bodies.circle(startX + 40, 200, 40);
+const snowball = Bodies.circle(startX + 40, 200, snowballStartingSize, {
+  render: {
+    sprite: {
+      texture: './images/snowball_small.png'
+    }
+  }
+});
 let hill = [];
+
+const getSnowballScale = () => {
+  const startingPayment = debtPayoffCalendar[0].currentExtraPayment;
+  const finalPayment =
+    debtPayoffCalendar[debtPayoffCalendar.length - 1].currentExtraPayment;
+  const previousMonthPayment =
+    debtPayoffCalendar[currentMonthIndex - 1].currentExtraPayment;
+  const currentMonthPayment =
+    debtPayoffCalendar[currentMonthIndex].currentExtraPayment;
+
+  const currentScale =
+    (currentMonthPayment - startingPayment) / (finalPayment - startingPayment) +
+    1;
+  const previousScale =
+    (previousMonthPayment - startingPayment) /
+      (finalPayment - startingPayment) +
+    1;
+
+  return currentScale - previousScale + 1;
+};
 
 const setup = () => {
   debtPayoffCalendar = debtPayoff();
   const hillSize = monthHillLength * debtPayoffCalendar.length;
 
-  hill = Bodies.rectangle(startX + hillSize / 2, startY, hillSize, 50, {
+  hill = Bodies.rectangle(hillSize / 2, hillSize / 2, hillSize, 50, {
     isStatic: true,
     angle: 0.3
   });
@@ -64,14 +91,15 @@ export const start = () => {
 
     if (currentMonthIndex >= debtPayoffCalendar.length) {
       Render.stop(render);
-    } else if (currentUpdate >= 120) {
+    } else if (currentUpdate >= 60) {
       currentMonthIndex++;
       currentUpdate = 0;
 
-      const scale =
-        debtPayoffCalendar[currentMonthIndex].currentExtraPayment / 500;
+      const scale = getSnowballScale();
 
       Body.scale(snowball, scale, scale);
+      snowball.render.sprite.xScale = scale;
+      snowball.render.sprite.yScale = scale;
     }
   };
 
